50d8bced0b154f00cd67c74c379146cb2abd5060
liciolentimo <lentimo@gmail.com>
2020-11-24 14:29:28 +0300

    added login form/activity

--- app/src/main/java/com/lentimosystems/dukalite/activities/LoginActivity.kt
+++ app/src/main/java/com/lentimosystems/dukalite/activities/LoginActivity.kt
@@ -1,18 +1,16 @@
 package com.lentimosystems.dukalite.activities
 
-import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.lentimosystems.dukalite.R
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
 
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
@@ -23,16 +21,5 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-        @Suppress("DEPRECATION")
-        Handler().postDelayed(
-            {
-                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-                finish()
-            },
-            3000
-        )
-
-//        val typeface: Typeface = Typeface.createFromAsset(assets,"Montserrat-Bold.ttf")
-//        tv_app_name.typeface = typeface
     }
 }
